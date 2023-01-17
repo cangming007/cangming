55d5ee19fe68e0aee2d24aafdc152fee34bcf2c4
yangjilai <yangjilai@baidu.com>
2023-01-17 17:20:02 +0800

    todolist antd chrome

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-## 
-开始
+##  部署个人博客到githubio
+npm run deploy
+
+## 本地调试
+npm start
--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,32 @@
-import React from 'react';
+import React from "react";
+import { Input, Button, List, Typography } from "antd";
 
 function App() {
-  return (
-    <div >
-     <div>
-      <input/>
-      <button>提交</button>
-     </div>
+  const data = [
+    "Racing car sprays burning fuel into crowd.",
+    "Japanese princess to wed commoner.",
+    "Australian walks 100km after outback crash.",
+    "Man charged over missing wedding girl.",
+    "Los Angeles battles huge wildfires.",
+  ];
 
-     <ul>
-      <li>CangMing</li>
-     </ul>
+  return (
+    <div style={{ marginTop: "20px", marginLeft: "10px" }}>
+      <div>
+        <Input
+          placeholder="input info"
+          style={{ width: "300px", marginRight: "5px" }}
+        />
+        <Button type="primary">提交</Button>
+      </div>
+
+      <List
+        style={{ width: "350px", marginTop: "10px" }}
+        size="small"
+        bordered
+        dataSource={data}
+        renderItem={(item) => <List.Item>{item}</List.Item>}
+      />
     </div>
   );
 }
